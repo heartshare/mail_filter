53c4d6178a75bb908c2b659cde028a6523f5bf6a
Jeff Reifman <jeff@newscloud.com>
2014-01-09 00:34:18 -0800

    update upgrade page

--- app/protected/views/site/pages/upgrade.php
+++ app/protected/views/site/pages/upgrade.php
@@ -1,9 +1,12 @@
 <h1>Upgrade to the Advanced Module</h1>
-<p>While the free, open source basic version of Filtered includes a complete mail filtering solution, the following features are only available in the advanced module:</p>
+<p>While the free, open source basic version of Filtered includes a <a href="http://jeffreifman.com/filtered-open-source-imap-mail-filtering-software-for-php/feature-summary/">complete imap mail filtering solution</a>, the following features are only available in the advanced module:</p>
 <ul>
   <li>Direct links from email digests to read message content on the web</li>
   <li>Quiet hours with exclusions. Set multiple periods where email will be held from your inbox e.g. weekends, late evenings with exclusions for important people such as family.</li>
   <li>Smartphone notifications via Pushover based on sender or subject keyword</li>
+  <li>White listing email challenges unknown senders to verify their identity</li>
+  <li>Privacy folder encrypts email to your web server</li>
+  <li>Routing via recipient mailbox aids management of using anonymous catch-all domains</li>
   <li>Automatic inbox archiving. Regularly moves messages of a specified age e.g. 1 month from inbox to Archive folder. Helps keep your inbox clean.</li>
   <li>Automatic expiration of sender's messages. Some emails just aren't important to keep. Automatically deletes messages after a set period of time e.g. 1 week, 1 month, 1 year for selected senders.</li>
 </ul>
